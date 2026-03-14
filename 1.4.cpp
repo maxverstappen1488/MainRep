13ae35e0293bb45e5294df1b9b6c2d5cf583914c
Максим Борисов ТКИ-242 <maks.borisov858@gmail.com>
2026-03-14 18:41:35 +0300

    Add files via upload

---
+++ 1.4.cpp
@@ -0,0 +1,46 @@
+#include <iostream> #include <cmath>
+using namespace std;
+
+
+double otr(const double a, const double b, const double c){
+    double k=0;
+    if (a < 0){
+        k=k+1;
+    }
+    if (b < 0){
+        k=k+1;
+    }
+    if (c < 0){
+        k=k+1;
+    }
+    return k;
+
+}
+double sum(const double a, const double b, const double c){
+    return a+b+c;
+
+}
+int main(){
+    double a = 0;
+    double b = 0;
+    double c = 0;
+    
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Введите третье число: ";
+    cin >> c;
+
+    double K = otr(a, b, c);
+    double S = sum(a, b, c);
+
+    //выводим
+
+    cout << "Количество отрицательных чисел: " << K << endl;
+    cout << "Сумма чисел: " << S << endl;
+    
+
+    return 0;
+}
+
