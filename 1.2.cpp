dc4b1084c85eb39b1549c91ba7e70fdb066300bb
Максим Борисов ТКИ-242 <maks.borisov858@gmail.com>
2026-03-22 11:59:50 +0300

    Update 1.2.cpp

--- 1.2.cpp
+++ 1.2.cpp
@@ -1,28 +1,26 @@
 #include <iostream>
 using namespace std;
-/** 
-* @brief вычисляет путь, пройденный лодкой по течению 
+/**
+* @brief вычисляет путь, пройденный лодкой по течению
 * @param river_speed - cкорость течения реки
 * @param boat_speed - cкорость лодки
 * @param time - время пути
 * @return - путь, пройденный лодкой
 */
-double calculateL (const double river_speed, const double boat_speed, const double t){
-    return (river_speed + boat_speed) * t;
-}
+double calculateL(const double river_speed, const double boat_speed, const double t);
 
-/** 
+/**
 * @brief - точка входа в программу
 * @return - 0, если программа выполнена корректно
 */
-int main(){
+int main() {
     // скорость реки
     double river_speed = 0;
     // скорость лодки
     double boat_speed = 0;
     // время пути
     double t = 0;
-    
+
     //вводим
     cout << "Введите скорость реки: ";
     cin >> river_speed;
@@ -30,20 +28,24 @@ int main(){
     cin >> boat_speed;
     cout << "Введите время пути: ";
     cin >> t;
-    
+
     //выводим
     cout << "Скорость реки: " << river_speed << endl;
     cout << "Скорость лодки: " << boat_speed << endl;
     cout << "Время пути: " << t << endl;
-    
+
     //путь
-    
+
     //формула: (river_speed + boat_speed) * t
-    
-    double L = calculateL (river_speed, boat_speed, t);
-    
+
+    double L = calculateL(river_speed, boat_speed, t);
+
     //результат
-    
+
     cout << "Путь: " << L << endl;
+    return 0;
+}
+double calculateL(const double river_speed, const double boat_speed, const double t) {
     
+    return (river_speed + boat_speed) * t;
 }
