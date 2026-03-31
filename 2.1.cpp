a2008a1170c6551194f77a1dac281195362063ff
Максим Борисов ТКИ-242 <maks.borisov858@gmail.com>
2026-04-01 01:15:08 +0300

    Add files via upload

---
+++ 2.1.cpp
@@ -0,0 +1,83 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/**
+ * @brief - подсчитывает количество отрицательных чисел
+ * @param a - первое число
+ * @param b - второе число
+ * @param c - третье число
+ * @return - количество отрицательных чисел
+ */
+double otr(const double a, const double b, const double c);
+
+/**
+ * @brief - вычисляет модуль суммы трех чисел
+ * @param a - первое число
+ * @param b - второе число
+ * @param c - третье число
+ * @return - модуль суммы чисел
+ */
+double sum(const double a, const double b, const double c);
+
+/**
+ * @brief - точка входа в программу
+ * @return 0, если программа выполнена корректно
+ */
+int main() {
+    // ввод
+    double a = 0;
+    double b = 0;
+    double c = 0;
+
+    cout << "Введите первое число: ";
+    cin >> a;
+    cout << "Введите второе число: ";
+    cin >> b;
+    cout << "Введите третье число: ";
+    cin >> c;
+
+    // вычисления
+    double K = otr(a, b, c);
+    double S = sum(a, b, c);
+
+    // вывод
+    cout << "Количество отрицательных чисел: " << K << endl;
+    cout << "Сумма чисел: " << S << endl;
+
+
+    return 0;
+}
+
+/**
+ * @brief - подсчитывает количество отрицательных чисел
+ * @param a - первое число
+ * @param b - второе число
+ * @param c - третье число
+ * @return - количество отрицательных чисел
+ */
+double otr(const double a, const double b, const double c) {
+    double k = 0;
+    if (a < 0) {
+        k = k + 1;
+    }
+    if (b < 0) {
+        k = k + 1;
+    }
+    if (c < 0) {
+        k = k + 1;
+    }
+    return k;
+}
+
+/**
+ * @brief - вычисляет модуль суммы трех чисел
+ * @param a - первое число
+ * @param b - второе число
+ * @param c - третье число
+ * @return - модуль суммы чисел
+ */
+double sum(const double a, const double b, const double c) {
+    return abs(a + b + c);
+
+}
